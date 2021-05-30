9a4ba762e93872f84b54867bb9a9a99d7eac1717
lkipland <lucyna.gamon@gmail.com>
2021-05-30 15:15:52 +0200

    pierwszy commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let tekst1 = "Uwielbiam JavaScript";
+let tekst2 = "Jestem świetnym programistą";
+
+if (tekst1.length > tekst2.length) { 
+    console.log (tekst1);
+} 
+
+else {
+    console.log (tekst2);
+}
